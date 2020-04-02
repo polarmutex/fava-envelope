3aaa1933482da57d1eabb39c9af573561a453348
Brian Ryall <brianryall@fastmail.com>
2020-04-02 10:42:50 -0400

    inital module files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from os import path
+from setuptools import find_packages, setup
+
+with open(path.join(path.dirname(__file__), 'README.md')) as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='fava-envelope',
+    version='1.0',
+    description='Fava Envelope budgeting for beancout',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/bryall/fava-envelope',
+    author='Brian RYall',
+    author_email='bryall@gmail.com',
+    license='MIT',
+    keywords='fava beancount accounting budgeting',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'beancount>=2.2.3',
+        'fava>=1.13'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Office/Business :: Financial :: Accounting'
+    ],
+)
