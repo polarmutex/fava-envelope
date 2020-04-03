c55b10129156dc8e28d53ee257e8e08de98c3fa5
Brian Ryall <brianryall@fastmail.com>
2020-04-02 20:35:10 -0400

    initial fava ext code

---
+++ fava_envelope/__init__.py
@@ -0,0 +1,12 @@
+"""
+"""
+
+from fava.ext import FavaExtensionBase
+
+class EnvelopeBudget(FavaExtensionBase):
+    '''
+    '''
+    report_title = "Envelope Budget"
+
+    def build_budget_tables(self, begin=None, end=None):
+        return ""
