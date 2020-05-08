8aba7f012abb5bca5268eeee9395dc1149ddfb29
Brian Ryall <brianryall@fastmail.com>
2020-05-08 16:20:57 -0400

    added code to produce envelope pages into fava

--- fava_envelope/__init__.py
+++ fava_envelope/__init__.py
@@ -12,24 +12,51 @@ class EnvelopeBudget(FavaExtensionBase):
     '''
     report_title = "Envelope Budget"
 
-    def build_envelope_budget_tables(self, begin=None, end=None):
+    def generate_budget_df(self):
         module = BeancountEnvelope(
             self.ledger.entries,
             self.ledger.options
         )
-        income_tables, envelope_tables = module.envelope_tables()
-        return self.generate_envelope_query_table(
-            income_tables, envelope_tables)
+        self.income_tables, self.envelope_tables = module.envelope_tables()
 
-    def generate_envelope_query_table(self, income_tables, envelope_tables):
+    def get_budgets_months_available(self):
+        self.generate_budget_df()
+        return self.income_tables.columns
 
-        tables = []
+    def generate_income_query_tables(self, month):
 
         income_table_types = []
-        income_table_types.append(("Funds for mmonth", str(Decimal)))
-        income_table_types.append(("Overspent in prev month", str(Decimal)))
-        income_table_types.append(("Budgeted for month", str(Decimal)))
-        income_table_types.append(("To be budgeted for month", str(Decimal)))
+        income_table_types.append(("Name", str(str)))
+        income_table_types.append(("Amount", str(Decimal)))
+
+        income_table_rows = []
+
+        if month is not None:
+            row = {}
+            income_table_rows.append({
+                "Name": "Funds for month",
+                "Amount": self.income_tables[month]["Avail Income"]
+            })
+            income_table_rows.append({
+                "Name": "Overspent in prev month",
+                "Amount": self.income_tables[month]["Overspent"]
+            })
+            income_table_rows.append({
+                "Name": "Budgeted for month",
+                "Amount":  self.income_tables[month]["Budgeted"]
+            })
+            income_table_rows.append({
+                "Name": "To be budgeted for month",
+                "Amount":  self.income_tables[month]["To Be Budgeted"]
+            })
+            income_table_rows.append({
+                "Name": "Budgeted in the future",
+                "Amount":  self.income_tables[month]["Budgeted Future"]
+            })
+
+        return (income_table_types, income_table_rows)
+
+    def generate_envelope_query_tables(self, month):
 
         envelope_table_types = []
         envelope_table_types.append(("Account", str(str)))
@@ -37,17 +64,10 @@ class EnvelopeBudget(FavaExtensionBase):
         envelope_table_types.append(("Activity", str(Decimal)))
         envelope_table_types.append(("Available", str(Decimal)))
 
-        income_table_rows = []
-        for month in income_tables.columns:
-            row = {}
-            row["Funds for month"] = income_tables[month]["Avail Income"]
-            row["Overspent in prev month"] = income_tables[month]["Overspent"]
-            row["Budgeted for month"] = income_tables[month]["Budgeted"]
-            row["To be budgeted for month"] = income_tables[month]["To Be Budgeted"]
-            income_table_rows.append(row)
+        envelope_table_rows = []
 
-            envelope_table_rows = []
-            for index, e_row in envelope_tables.iterrows():
+        if month is not None:
+            for index, e_row in self.envelope_tables.iterrows():
                 row = {}
                 row["Account"] = index
                 row["Budgeted"] = e_row[month, "budgeted"]
@@ -55,16 +75,4 @@ class EnvelopeBudget(FavaExtensionBase):
                 row["Available"] = e_row[month, "available"]
                 envelope_table_rows.append(row)
 
-            tables.append((
-                month,
-                {
-                    "income_table": (
-                        income_table_types,
-                        income_table_rows),
-                    "envelope_table": (
-                        envelope_table_types,
-                        envelope_table_rows)
-                }
-            ))
-
-        return tables
+        return (envelope_table_types, envelope_table_rows)
