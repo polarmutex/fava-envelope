01e4e71080709917354059779b0f239b5b17ed26
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2021-04-16 16:38:12 +0300

    Removing a stray, unused variable

--- fava_envelope/__init__.py
+++ fava_envelope/__init__.py
@@ -34,7 +34,6 @@ class EnvelopeBudget(FavaExtensionBase):
         
     
     def get_currencies(self):
-        titles=[]
         try:
             return self.config["currencies"]
         except:
