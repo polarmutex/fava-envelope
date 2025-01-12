7ef21e01fb09d09b7bff71eced0f1f2357d6e03b
Brian Ryall <me@brianryall.xyz>
2025-01-12 15:13:04 -0500

    chore: update nix and switch to uv

--- flake.nix
+++ flake.nix
@@ -2,125 +2,138 @@
   description = "nix derivation for fava-envelope";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    pyproject-nix.url = "github:nix-community/pyproject.nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
-    self,
+    # self,
+    flake-parts,
     nixpkgs,
-    flake-utils,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
     ...
   } @ inputs:
-    {}
-    // (flake-utils.lib.eachSystem [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
+    flake-parts.lib.mkFlake
+    {inherit inputs;}
+    {
+      systems = [
         "x86_64-linux"
-      ]
-      (system: let
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {system, ...}: let
+        inherit (nixpkgs) lib;
         pkgs = import nixpkgs {
           inherit system;
-          # config.allowUnsupportedSystem = true;
-          overlays = [
-            (
-              _: prev: {
-                pythonPackagesExtensions =
-                  prev.pythonPackagesExtensions
-                  ++ [
-                    (py-final: _: {
-                      fava = py-final.buildPythonPackage rec {
-                        pname = "fava";
-                        version = "1.27.3";
-                        format = "pyproject";
-                        src = python.pkgs.fetchPypi {
-                          inherit pname version;
-                          sha256 = "sha256-GsnXZaazEiOhyjbIinHRD1fdoqlAp3d5csrmtydxmGM=";
-                        };
-                        nativeBuildInputs = with python.pkgs; [setuptools-scm];
-                        propagatedBuildInputs = with python.pkgs; [
-                          babel
-                          beancount
-                          cheroot
-                          click
-                          flask
-                          flask-babel
-                          jaraco-functools
-                          jinja2
-                          markdown2
-                          ply
-                          simplejson
-                          werkzeug
-                        ];
-                        postPatch = ''
-                          substituteInPlace pyproject.toml \
-                            --replace 'setuptools_scm>=8.0' 'setuptools_scm'
-                        '';
-                      };
-                    })
-                  ];
-              }
+          overlays = [];
+        };
+
+        workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+        };
+
+        editableOverlay = workspace.mkEditablePyprojectOverlay {
+          root = "$REPO_ROOT";
+        };
+
+        # Python sets grouped per system
+        pythonSets = let
+          # Base Python package set from pyproject.nix
+          baseSet = pkgs.callPackage pyproject-nix.build.packages {
+            python = pkgs.python312;
+          };
+
+          # An overlay of build fixups & test additions
+          pyprojectOverrides = _final: prev: {
+          };
+
+          buildSystemOverrides = final: prev: let
+            inherit (final) resolveBuildSystem;
+            inherit (builtins) mapAttrs;
+            # Build system dependencies specified in the shape expected by resolveBuildSystem
+            # The empty lists below are lists of optional dependencies.
+            #
+            # A package `foo` with specification written as:
+            # `setuptools-scm[toml]` in pyproject.toml would be written as
+            # `foo.setuptools-scm = [ "toml" ]` in Nix
+            buildSysOverrides = {
+              # curlify.setuptools = [];
+              # fava-dashboards = {
+              #   hatchling = [];
+              #   hatch-vcs = [];
+              # };
+              # fava-envelope.pdm = [];
+              # ofxparse.setuptools = [];
+              # ofxtools.setuptools = [];
+            };
+          in
+            mapAttrs (
+              name: spec:
+                prev.${name}.overrideAttrs (old: {
+                  nativeBuildInputs = old.nativeBuildInputs ++ resolveBuildSystem spec;
+                })
             )
-          ];
-        };
-
-        project = inputs.pyproject-nix.lib.project.loadPyproject {
-          projectRoot = ./.;
-        };
-
-        python = pkgs.python3;
-      in {
-        devShell = let
-          path = "~/repos/personal/fava-envelope/devel";
-          entrypoints = {};
-          editablePackage = python.pkgs.toPythonModule (
-            pkgs.runCommand "fava-envelope-editable"
-            {} ''
-              mkdir -p "$out/${python.sitePackages}"
-              cd "$out/${python.sitePackages}"
-
-              # See https://docs.python.org/3.8/library/site.html for info on such .pth files
-              # These add another site package path for each line
-              echo '${toString path}' > fava-envelope-editable.pth
-
-              # Create a very simple egg so pkg_resources can find this package
-              # See https://setuptools.readthedocs.io/en/latest/formats.html for more info on the egg format
-              mkdir "fava-envelope.egg-info"
-              cd "fava-envelope.egg-info"
-
-              # Just enough standard PKG-INFO fields for an editable installation
-              cat > PKG-INFO <<EOF
-              Metadata-Version = "2.1";
-              Name = fava-envelope;
-              Version = 0.5.8;
-              Summary = "";
-              EOF
-
-              ${pkgs.lib.optionalString (entrypoints != {}) ''
-                cat > entry_points.txt <<EOF
-                ${pkgs.lib.generators.toINI {} entrypoints}
-                EOF
-              ''}
-            ''
+            buildSysOverrides;
+        in
+          baseSet.overrideScope (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+              pyprojectOverrides
+              buildSystemOverrides
+            ]
           );
-          arg = project.renderers.withPackages {inherit python;};
-          pythonEnv = python.withPackages arg;
+      in {
+        devShells.default = let
+          pkgs = nixpkgs.legacyPackages.${system};
+          editablePythonSet = pythonSets.overrideScope editableOverlay;
+          venv = editablePythonSet.mkVirtualEnv "fava-envelope-dev-env" {
+            fava-envelope = ["dev"];
+          };
         in
           pkgs.mkShell {
-            # venvDir = "./.venv";
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = "${venv}/bin/python";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
             packages = with pkgs; [
-              pythonEnv
-              python.pkgs.black
-              # python3Packages.virtualenv
-              # python3Packages.venvShellHook
+              venv
+              uv
             ];
             shellHook = ''
-              venv="$(cd $(dirname $(which python)); cd ..; pwd)"
-              ln -Tsf "$venv" .venv
-              export PYTHONPATH=./src:$PYTHONPATH
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
             '';
           };
-      }));
+        packages = {
+        };
+      };
+    };
 }
